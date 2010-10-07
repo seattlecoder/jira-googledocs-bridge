2144050af44ddce007e30216af34639945cce8b8
ysm906 <ysm906@48294c84-ddda-e3a4-df6b-ab849c33e4b8>
2010-10-07 04:02:40 +0000

    code update

--- trunk/google-docs-jira.py
+++ trunk/google-docs-jira.py
@@ -3,6 +3,7 @@
 import gdata.docs.data
 import gdata.docs.client
 import SOAPpy
+import re
 from xml.dom.minidom import parseString
 
 
@@ -23,19 +24,25 @@ def getFolderUri(folderName):
 ## path: file path in the local machine
 ## file: file name
 def upload(path, file):
-  entry = client.Upload(media=path, title=file, folder_or_uri=getFolderUri('GoogleDocsJira'), content_type='text/plain')
+  #entry = client.Upload(media=path, title=file, folder_or_uri=getFolderUri('GoogleDocsJira'), content_type='application/msword')
+  entry = client.Upload(media=path, title=file, content_type='application/msword')
 
   print 'Document \'%s\' uploaded.' % file
 
 ### download
 ## resourceid: google document id
 def download(resourceid):
+  file_ext = '.tmp'
+  print resourceid
   entry = client.GetDoc(resourceid)
 
   # download with tmp extension
-  client.Download(entry,file_path='%s.tmp'%entry.title.text)
+  #client.Download(entry, file_path='%s' % entry.title.text+file_ext)
 
-  print 'Document \'%s\' downloaded as \'%s.tmp\'.' % (entry.title.text, entry.title.text)
+  file_path = '%s' % entry.title.text+file_ext
+  client.Export(entry, file_path)
+
+  print 'Document \'%s\' downloaded as \'%s\'.' % (entry.title.text, entry.title.text+file_ext)
 
 ### create a document with content and upload it to google docs
 ## fname: file name
@@ -66,13 +73,20 @@ def downloadDoc(title):
 
 ### get content from html format
 def getContentFromFile(fname):
-  lt = '&lt;'
-  gt = '&gt;'
-
   # read file
   file = open(fname,'r')
   str = file.read()
   file.close()
+
+  #return str
+
+  keys = getKeyList(str)
+  queries = getQueryList(str)
+
+### replace html entity
+def replaceHtmlEntity(str):
+  lt = '&lt;'
+  gt = '&gt;'
 
   # get start / end index of content
   start = str.index(lt)
@@ -84,6 +98,48 @@ def getContentFromFile(fname):
   substr = substr.replace(gt,'>')
 
   return substr
+
+### get jira key from text contents
+def getKeyList(string):
+  keyList = []
+
+  jiraMatchObj = re.findall(r'&lt;jira&gt;FG-\d*&lt;/jira&gt;', string, re.M|re.I)
+  if jiraMatchObj:
+    for match in jiraMatchObj:
+      match = replaceHtmlEntity(match)
+
+      dom = parseString(match)
+      jiras = dom.getElementsByTagName('jira')
+      for jira in jiras:
+        nodes = jira.childNodes
+        for node in nodes:
+          if node.nodeType == node.TEXT_NODE:
+            issuekey = node.data
+            keyList.append(issuekey)
+
+  return keyList
+      
+
+### get jira query from text contents
+def getQueryList(string):
+  queryList = []
+  jiralistMatchObj = re.findall(r'&lt;jiralist&gt;.*&lt;/jiralist&gt;', string, re.M|re.I)
+  if jiralistMatchObj:
+    for match in jiralistMatchObj:
+      match = replaceHtmlEntity(match)
+
+      dom = parseString(match)
+      jiralists = dom.getElementsByTagName('jiralist')
+      for jiralist in jiralists:
+        print 'node name: '+jiralist.nodeName
+        nodes = jiralist.childNodes
+        for node in nodes:
+          if node.nodeType == node.TEXT_NODE:
+            print 'node data: '+node.data
+            query = node.data
+            queryList.append(query)
+
+  return queryList
 
 ### test update content
 def updateContent(content):
@@ -108,33 +164,15 @@ def updateContent(content):
   query = ''
 
   # get key value
-  testContent = '<issue><jira>FG-111</jira><jira>FG-101</jira><jiralist>summary ~ portal</jiralist></issue>'
-  dom = parseString(testContent)
-  jiras = dom.getElementsByTagName('jira')
-  for jira in jiras:
-    print 'node name: '+jira.nodeName
-    nodes = jira.childNodes
-    for node in nodes:
-      if node.nodeType == node.TEXT_NODE:
-        print 'node data: '+node.data
-        issuekey = node.data
-        
+
         issue = soap.getIssue(auth, issuekey)
 
         #print issue key
         jiraIssue = issue.key+':'+issue.summary+' '+issue.priority+' '+issue.assignee+' Due:'+str(issue.duedate[0:3])
         print jiraIssue
-        testContent = testContent.replace(issuekey, jiraIssue)
+        content = content.replace(issuekey, jiraIssue)
 
   # get query
-  jiralists = dom.getElementsByTagName('jiralist')
-  for jiralist in jiralists:
-    print 'node name: '+jiralist.nodeName
-    nodes = jiralist.childNodes
-    for node in nodes:
-      if node.nodeType == node.TEXT_NODE:
-        print 'node data: '+node.data
-        query = node.data
 
         issues = soap.getIssuesFromJqlSearch(auth, query, 3)
         #print issues
@@ -179,9 +217,9 @@ def updateContent(content):
               jiraIssueList = jiraIssueList + format % ('', '', '', list[idx], '', '', '', '', '', '') + '\n'
               idx = idx + 1
 
-        testContent = testContent.replace(query, jiraIssueList)
+        content = content.replace(query, jiraIssueList)
 
-  print 'updated content: '+testContent
+  print 'updated content: '+content
   print 'Content updated.'
 
   return content
@@ -219,8 +257,8 @@ client.ClientLogin(gmail, gpasswd, client.source, 'writely')
 
 #createDoc('file-edit')
 
-#downloadDoc('file-edit')
-content = getContentFromFile('file-edit.tmp')
-content = updateContent(content) # this is test
+#downloadDoc('myTest')
+content = getContentFromFile('myTest.tmp')
+#content = updateContent(content) # this is test
 #writeContent(content, 'file-view')
-#uploadDoc('file-view')
+#uploadDoc('myTest.tmp')
